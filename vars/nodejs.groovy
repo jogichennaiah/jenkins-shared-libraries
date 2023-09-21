49377ecbe148746fe159b4460798a1aed9db7b26
jogichennaiah <chennaiahjogi394@gmail.com>
2023-09-21 22:42:03 +0530

    Auto push

---
+++ vars/nodejs.groovy
@@ -0,0 +1,27 @@
+def call() {
+
+
+    pipeline {
+        agent any 
+        stages {
+            stage('Lint Checks') {
+                 steps {
+
+                    sh "echo Installing JSlist"
+                    sh "npm i jslint"
+                    sh "echo Starting linkChecks...."
+                    sh "node_modules/jslint/bin/jslint.js server.js || true"
+                    sh "echo linkCheck completed"
+                }
+                    }
+            stage('Generating Artifacts') {
+                steps {
+                    sh "echo Generating artifacts...."
+                    sh "npm install"
+                
+                 }
+            }
+
+        } 
+     }
+} 
