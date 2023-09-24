7c954940fd525c6372457a39c685af3b75f4f9e6
jogichennaiah <chennaiahjogi394@gmail.com>
2023-09-24 19:30:04 +0530

    Auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -23,20 +23,48 @@ def call() {
                     } 
                 }
             }
-            stage('Sonar Checks') {
-                steps {
-                    sh "echo Starting Sonar Checks For ${COMPONENT}"
-                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}" 
-                    sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > qyality-gate.sh"
-                    sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${COMPONENT}"
-                    sh "echo  ${COMPONENT} Sonar Checks are completed" 
+            stage('Test Cases') {
+                parallel {
+                    stage('Unit Testing') {
+                        steps {
+                            sh "echo Starting Unit Testing"
+                            sh "echo Unit Testing Completed"
+                        }
+                    }
+                    stage('Integration Testing') {
+                        steps {
+                            sh "echo Starting Integration Testing"
+                            sh "echo Integration Testing Completed"
+                        }
+                    }
+                    stage('Functional Testing') {
+                        steps {
+                            sh "echo Starting Functional Testing"
+                            sh "echo Functional Testing Completed"
+                        }
+                    }
                 }
             }
+ //           stage('Sonar Checks') {
+//                steps {
+//                    sh "echo Starting Sonar Checks For ${COMPONENT}"
+ //                   sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}" 
+//                    sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > qyality-gate.sh"
+ //                   sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${COMPONENT}"
+ //                   sh "echo  ${COMPONENT} Sonar Checks are completed" 
+ //               }
+ //           }
             stage('Generating Artifacts') {
+                when 
                 steps {
                     sh "echo Generating artifacts...."
                     sh "npm install"
                 
+                 }
+            }
+            stage('Uploading Artifacts') {
+                steps {
+                    sh "echo Uploading Artifacts..." 
                  }
             }
         } 
