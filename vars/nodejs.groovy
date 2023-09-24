6611a6f71bd2fe184aa0c3d7fb8754ebdfee1964
jogichennaiah <chennaiahjogi394@gmail.com>
2023-09-24 11:47:56 +0530

    Auto push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -25,8 +25,11 @@ def call() {
             }
             stage('Sonar Checks') {
                 steps {
-                    sh "env"
-                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"  
+                    sh "echo Starting Sonar Checks For ${COMPLETED}"
+                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}" 
+                    sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > qyality-gate.sh"
+                    sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${COMPONENT}"
+                    sh "echo  ${COMPONENT} Sonar Checks are completed" 
                 }
             }
             stage('Generating Artifacts') {
