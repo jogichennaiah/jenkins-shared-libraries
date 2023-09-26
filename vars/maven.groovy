192d86174206e3afa7e98d0b23e0a6274fcac29b
jogichennaiah <chennaiahjogi394@gmail.com>
2023-09-26 23:09:29 +0530

    Auto push

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -26,7 +26,55 @@ def call() {
                 
                  }
             }
+            stage('Sonar Checks') {
+                steps {
+                    script {
+                            env.ARGS="-Dsonar.java.binaries=target/"
+                            common.sonarChecks()
+                        }
+                    }
+                }
+            stage('Test Cases') {
+                parallel {
+                    stage('Unit Testing') {
+                        steps {
+                            sh "echo Starting Unit Testing"
+                            sh "echo Unit Testing Completed"
+                        }
+                    }
+                    stage('Integration Testing') {
+                        steps {
+                            sh "echo Starting Integration Testing"
+                            sh "echo Integration Testing Completed"
+                        }
+                    }
+                    stage('Functional Testing') {
+                        steps {
+                            sh "echo Starting Functional Testing"
+                            sh "echo Functional Testing Completed"
+                        }
+                    }
+                }
+            }
+            stage('Check The Release') {
+                when {
+                    expression { env.TAG_NAME != null }
+                }
+                steps {
+                    script {
+                        env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true")
+                        print UPLOAD_STATUS
+                    }
+                }
+            }
+            stage('Generating Artifacts') {
+                steps {
+                    sh "echo Artifact Generation Complete"
+                    }
+                }
+            }
+        }
+    }
 
-        } 
-     }
-} 
+        
+     
