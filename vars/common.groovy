fa3546d2987ace226824dea445d0a7d32478146f
jogichennaiah <chennaiahjogi394@gmail.com>
2023-09-27 23:48:49 +0530

    Auto push

--- vars/common.groovy
+++ vars/common.groovy
@@ -6,31 +6,7 @@ def sonarChecks() {
             //sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${COMPONENT}"
           sh "echo  ${COMPONENT} Sonar Checks are completed" 
         }
-}    
-
-def lintChecks() {
-    stage('Lint Checks') {
-        if(env.APPTYPE == "nodejs") {
-            sh "echo Installing JSlist"
-            sh "npm i jslint"
-            sh "echo starting linkChecks for ${COMPONENT}"
-            sh "node_modules/jslint/bin/jslint.js server.js || true"
-            sh "echo linkChecks completed for ${COMPONENT}"
-        }
-        else if(env.APPTYPE == "maven") {
-            sh "echo starting linkChecks for ${COMPONENT}"
-            sh "mvn checkstyle:check || true"
-            sh "echo linkChecks completed for ${COMPONENT}"
-        }
-        else if(env.APPTYPE == "pylint") {
-            sh "echo starting linkChecks for ${COMPONENT}"
-            sh "echo linkChecks completed for ${COMPONENT}"
-        }
-        else {
-            sh "echo Lint Checks For Frontend are in progress"
-        }   
-    }
-}
+}   
 
 def lintChecks() {
     stage('Lint Checks') {
