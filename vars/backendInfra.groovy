1972a54a5f0c4bf5ca74d1005ec5c7c63d3cc678
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-12 23:07:05 +0530

    Auto push

---
+++ vars/backendInfra.groovy
@@ -0,0 +1,37 @@
+def call() {
+    properties([
+            parameters([
+                choice(choices: 'dev\nprod', description: "Select your environment", name: "ENV"),
+                choice(choices: 'apply\ndestroy', description: "Chose an action", name: "ACTION"),
+                string(choices: 'APP_VERSION', description: "Enter your backend version", name: "APP_VERSION")
+            ]),
+        ])
+    node {
+        ansiColor('xterm') {
+            git branch: 'main', url: "https://github.com/jogichennaiah/${REPONAME}.git"
+            
+            stage('terraform init') {
+                sh ''' 
+                    cd ${TFDIR}
+                    ls -ltr
+                    terrafile -f env-${ENV}/Terrafile
+                    terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars  
+                '''       
+            }
+
+            stage('terraform plan') {
+                sh '''
+                    cd ${TFDIR}
+                    terraform plan -var-file=env-${ENV}/${ENV}.tfvars  -var APP_VERSION=${APP_VERSION} 
+                ''' 
+            }
+
+            stage('Terraform Action') {
+                sh '''
+                    cd ${TFDIR}
+                    terraform ${ACTION} -auto-approve -var-file=env-${ENV}/${ENV}.tfvars -var APP_VERSION=${APP_VERSION}
+                '''
+                }
+            }
+        }
+    }
