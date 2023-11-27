9e85dbe4bbcad799cd681b22487e82812a2acd28
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-27 22:43:52 +0530

    Auto push

--- vars/common.groovy
+++ vars/common.groovy
@@ -89,7 +89,7 @@ def artifacts() {
         stage('Uploading the artifacts') {
             withCredentials([usernamePassword(credentialsId: 'NEXUS_CRED', passwordVariable: 'NEXUS_PSW', usernameVariable: 'NEXUS_USR')]) {
                     sh "echo Uploading ${COMPONENT} artifact to nexus"
-                    sh "curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.93.233:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
+                    sh "curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.81.211:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
                     sh "echo Uploading ${COMPONENT} artifact to nexus is completed"
             }                        
         }
